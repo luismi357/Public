752838d9f0d26c86cc568969005d4054299ba464
luismi357 <luismiortegasancho@gmail.com>
2017-01-24 18:52:09 +0100

    Add files via upload

---
+++ prac2WebStorage/Ejer2WebStorage.js
@@ -0,0 +1,29 @@
+onload=function(){
+  var formulario = document.forms[0];
+  formulario.onsubmit = function(){
+
+  localStorage.nombre=nombre.value;
+  localStorage.apellidos=apellidos.value;
+  sessionStorage.color=color.value;
+  if(H.checked){
+    localStorage.sexo="H";
+  }else if(M.checked){
+    localStorage.sexo="M";
+  }
+  sessionStorage.poblacion=poblacion.value;
+  if(selector.value=="C"){
+    sessionStorage.selector="Castellon";
+  }else if(selector.value=="V"){
+    sessionStorage.selector="Valencia";
+  }else if(selector.value=="A"){
+    sessionStorage.selector="Alicante";
+  }
+
+  if(foro.checked==true){
+    formulario.action="foro.html";
+  }else if(eventos.checked==true){
+    formulario.action="eventos.html"
+  }
+
+  }
+}
